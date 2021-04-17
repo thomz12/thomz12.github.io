61c76c7c029a67ce3a89395083570679d0f97c40
thom <thom@stage-games.com>
2021-04-18 01:06:41 +0900

    Added physics test page

---
+++ PhysicsTest/Tiled/RPG.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.1" name="RPG" tilewidth="16" tileheight="16" tilecount="516" columns="12">
+ <image source="RPG Tileset.png" width="192" height="688"/>
+ <terraintypes>
+  <terrain name="Grass" tile="0"/>
+ </terraintypes>
+ <tile id="6" terrain=",,,0"/>
+ <tile id="7" terrain=",,0,0"/>
+ <tile id="8" terrain=",,0,"/>
+ <tile id="18" terrain=",0,,0"/>
+ <tile id="19" terrain="0,0,0,0"/>
+ <tile id="20" terrain="0,,0,"/>
+ <tile id="30" terrain=",0,,"/>
+ <tile id="31" terrain="0,0,,"/>
+ <tile id="32" terrain="0,,,"/>
+ <tile id="78" terrain="0,0,0,"/>
+ <tile id="79" terrain="0,0,,0"/>
+ <tile id="90" terrain="0,,0,0"/>
+ <tile id="91" terrain=",0,0,0"/>
+</tileset>
